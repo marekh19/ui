58e69249e68fc7433673252103457a444c6f7724
Marek Honzal <marek.honzal@applifting.cz>
2023-10-30 17:25:42 +0100

    feat: implemented tailwindcss for styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,14 @@ import React from "react";
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 const Button: React.FC<Props> = ({ children, ...props }: Props) => {
-  return <button {...props}>{children}</button>;
+  return (
+    <button
+      {...props}
+      className="text-3xl bg-emerald-300 text-stone-900 rounded-lg px-4 py-2"
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
